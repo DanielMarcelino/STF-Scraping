63b19038e8b7cabd3e029599928920f8b37b65ac
Daniel Marcelino <danielmarcelinoazevedo@gmail.com>
2021-10-17 10:37:18 -0300

    Modificação no nome dos metodos de teste da classe data

--- testes/teste_data.py
+++ testes/teste_data.py
@@ -11,76 +11,76 @@ def test_valida_data_que_respeita_o_formato_separador_intervalo():
     assert data.valida_data() == True
 
 
-def teste_valida_data_em_que_o_dia_esta_no_limite_maximo():
+def test_valida_data_em_que_o_dia_esta_no_limite_maximo():
     data = Data("31-01-0001")
     assert data.valida_data() == True
     
 
-def teste_valida_data_em_que_o_mes_esta_no_limite_maximo():
+def test_valida_data_em_que_o_mes_esta_no_limite_maximo():
     data = Data("31-12-0001")
     assert data.valida_data() == True
     
 
-def teste_valida_data_em_que_o_ano_esta_no_limite_maximo():
+def test_valida_data_em_que_o_ano_esta_no_limite_maximo():
     data = Data("31-12-9999")
     assert data.valida_data() == True
     
 
-def teste_invalida_data_em_que_o_dias_esta_acima_do_limite_maximo():
+def test_invalida_data_em_que_o_dias_esta_acima_do_limite_maximo():
     data = Data("32-12-9999")
     assert data.valida_data() == False
     
     
-def teste_invalida_data_em_que_o_mes_esta_acima_do_limite_maximo():
+def test_invalida_data_em_que_o_mes_esta_acima_do_limite_maximo():
     data = Data("31-13-9999")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_ano_esta_acima_do_limite_maximo():
+def test_invalida_data_em_que_o_ano_esta_acima_do_limite_maximo():
     data = Data("31-12-10000")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_dias_esta_abaixo_do_limite_maximo():
+def test_invalida_data_em_que_o_dias_esta_abaixo_do_limite_maximo():
     data = Data("00-12-9999")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_mes_esta_abaixo_do_limite_maximo():
+def test_invalida_data_em_que_o_mes_esta_abaixo_do_limite_maximo():
     data = Data("31-00-9999")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_ano_esta_abaixo_do_limite_maximo():
+def test_invalida_data_em_que_o_ano_esta_abaixo_do_limite_maximo():
     data = Data("31-12-0000")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_dia_contem_somente_uma_casa_decimal_sem_zero():
+def test_invalida_data_em_que_o_dia_contem_somente_uma_casa_decimal_sem_zero():
     data = Data("5-12-2021")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_o_mes_contem_somente_uma_casa_decimal_sem_zero():
+def test_invalida_data_em_que_o_mes_contem_somente_uma_casa_decimal_sem_zero():
     data = Data("05-1-2021")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_contem_separadores_inesperados():
+def test_invalida_data_em_que_contem_separadores_inesperados():
     data = Data("05/12/2021")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_contem_o_primeiro_separador_inesperado():
+def test_invalida_data_em_que_contem_o_primeiro_separador_inesperado():
     data = Data("05/12-2021")
     assert data.valida_data() == False
     
 
-def teste_invalida_data_em_que_contem_o_segundo_separador_inesperado():
+def test_invalida_data_em_que_contem_o_segundo_separador_inesperado():
     data = Data("05-12/2021")
     assert data.valida_data() == False
-    
 
-def teste_formata_data_corretamente():
+
+def test_formata_data_corretamente():
     data = Data("05-12-2021")
     assert data.data_formatada() == "05/12/2021"
