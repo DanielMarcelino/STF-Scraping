ec0525726cbbe3bbf0820a29d67f167720aeb23c
Daniel Marcelino <daniel.azevedo@justicafacil.com.br>
2021-11-22 12:25:42 -0300

    Reducao da classe Data em uma unica funcao

--- stf_scraping.py
+++ stf_scraping.py
@@ -1,58 +1,17 @@
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
+from datetime import datetime
 import hashlib
 import sys
 
 
-class Data:
-    def __init__(self, data: str):
-        self.__data = data
-
-    def __checa_tamanho(self):
-        return len(self.__data) == 10
-
-    def __checa_separador(self):
-        try:
-            separador_1 = (self.__data[2] == '-') or (self.__data[2] == '/')
-            separador_2 = (self.__data[5] == '-') or (self.__data[5] == '/')
-            return separador_1 and separador_2
-        except:
-            return False
-
-    def __checa_dia(self):
-        try:
-            dia = "{}{}".format(self.__data[0], self.__data[1])
-            dia = int(dia)
-            return 0 < dia <= 31
-        except:
-            return False
-
-    def __checa_mes(self):
-        try:
-            mes = "{}{}".format(self.__data[3], self.__data[4])
-            mes = int(mes)
-            return 0 < mes <= 12
-        except:
-            return False
-
-    def __checa_ano(self):
-        try:
-            ano = "{}{}{}{}".format(self.__data[6], self.__data[7], self.__data[8], self.__data[9])
-            ano = int(ano)
-            return 0 < ano <= 9999
-        except:
-            return False
-
-    def valida_data(self):
-        if self.__checa_tamanho() and self.__checa_separador() and self.__checa_dia() and self.__checa_mes() and  self.__checa_ano():
-            return True
-        else:
-            return False
-
-    def data_formatada(self):
-        return self.__data.replace("-", "/")
-
+def valida_data(data):
+    try:
+        datetime.strptime(data, "%d/%m/%Y")
+        return data
+    except:
+        return None
 
 class Diario:
     def __init__(self, data):
@@ -141,19 +100,16 @@ class Diario:
 
 def main(args):
     try:
-        data_publicacao = Data(args[1])
+        data = args[1]
     except:
         print("Nenhuma data foi informada.")
         exit()
 
-    if not data_publicacao.valida_data():
-        print("Data inválida!", end="\n\n")
-        print("-- Instrução --")
-        print("Formato da data: DD-MM-AAAA")
-        print("Separadores esperados: '-' ou '/'")
+    if not valida_data(data):
+        print("Data inválida!", end="\n")
         exit()
 
-    busca_diario = Diario(data_publicacao.data_formatada())
+    busca_diario = Diario(data)
 
     if not busca_diario.inicia_busca():
         print("Não há diários publicados na data informada.")
