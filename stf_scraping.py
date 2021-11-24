a7301dd1057d3de1a902f0af4c5600c5963560f3
Daniel Marcelino <daniel.azevedo@justicafacil.com.br>
2021-11-23 23:56:07 -0300

    Refatora todo o código

--- stf_scraping.py
+++ stf_scraping.py
@@ -1,114 +1,85 @@
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
+import requests
+import re
 from bs4 import BeautifulSoup
 from datetime import datetime
 import hashlib
-import sys
+import argparse
 
+class Scraper:
+    def __init__(self, data:str) -> None:
+        self._lista_urls_pdfs = self.__obtem_urls_dos_pdfs_da_pagina_html(data)
 
-def valida_data(data):
-    try:
-        data = data.replace("-", "/")
-        datetime.strptime(data, "%d/%m/%Y")
-        return data
-    except:
-        return None
+    prefixo_url = 'https://www.stf.jus.br/portal/diariojusticaeletronico/'
+    posfixo_url = 'montarDiarioEletronico.asp?tp_pesquisa=0&dataP='
+    headers = {'user-agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
 
-class Diario:
-    def __init__(self, data):
-        self.__data = data
-        self.__lista_url_pdf = []
-        self.__html = ""
-        self.__headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'}
-        self.__lista_binarios_pdf = []
-        self.__lista_md5 = []
+    @property
+    def lista_urls_pdf(self) -> list:
+        return self._lista_urls_pdfs[:]
 
-    def __verifica_url_captura_response(self, url):
-        try:
-            request = Request(url, headers=self.__headers)
-            response = urlopen(request)
-            return  response.read()
-        except HTTPError as error:
-            print(error.status, error.reason)
-            exit()
-        except URLError as error:
-            print(error.reason, "\nVerifique sua conexão com a internet.")
-            exit()
+    def __obtem_urls_dos_pdfs_da_pagina_html(self, data:str) -> list:
+        pagina_html = self.__obtem_pagina_html(data)
+        pagina_html = BeautifulSoup(pagina_html, 'html.parser')
+        links = pagina_html.find_all('a', target='_blank')
+        lista_urls_pdfs = [(f'{Scraper.prefixo_url}{link.get("href")}') for link in links]
+        return lista_urls_pdfs
 
-    def __checa_existencia_de_diario(self):
-        try:
-            url = 'https://www.stf.jus.br/portal/diariojusticaeletronico/montarDiarioEletronico.asp?tp_pesquisa=0&dataP='
-            url = url + self.__data
-            self.__html = self.__verifica_url_captura_response(url)
-            return self.__html != b"0|"
-        except:
-            return False
+    def __obtem_pagina_html(self, data:str) -> str:
+        url = f'{Scraper.prefixo_url}{Scraper.posfixo_url}{data}'
+        pagina_html = self.__obtem_response(url)
+        pagina_html = self.__sanitiza_html(pagina_html)
+        return pagina_html
+        
+    def __obtem_response(self, url:str) -> str:
+        response = requests.get(url, headers = Scraper.headers)
+        return response.text
 
-    def __trata_html(self):
-        self.__html = self.__html.decode('utf-8')
-        self.__html = self.__html.split()
-        self.__html = " ".join(self.__html)
-        self.__html = self.__html.replace('" ', '"')
-        self.__html = self.__html.replace(' "', '"')
+    def __sanitiza_html(self, pagina_html:bytes) -> str:
+        pagina_html = re.sub(r'[\n|\t|\r]', '', pagina_html)
+        return pagina_html
 
-    def get_url_pdf(self):
-        self.__trata_html()
-        self.__html = BeautifulSoup(self.__html, 'html.parser')
-        self.__html = self.__html.find_all('a', target='_blank')
-        for link in self.__html:
-            link = link.get("href")
-            self.__lista_url_pdf.append(link)
-        return self.__lista_url_pdf
+class PDFDownloader:
+    def __init__(self, lista_urls_pdf:list) -> None:
+        self._lista_md5_pdfs = self.__obtem_arquivos_pdfs(lista_urls_pdf)
 
-    def __gera_lista_arquivos_binarios_pdf(self):
-        prefixo_url = 'http://www.stf.jus.br/portal/diariojusticaeletronico/'
-        for url in self.__lista_url_pdf:
-            url = prefixo_url + url
-            self.__lista_binarios_pdf.append(self.__verifica_url_captura_response(url))
+    def __obtem_arquivos_pdfs(self, lista_urls_pdfs:list) -> list:
+        lista_md5_pdfs = []
+        for url in lista_urls_pdfs:
+            binario_pdf = self.__obtem_response(url)
+            md5_pdf = self.__obtem_md5_pdf(binario_pdf)
+            self.__salva_arquivo_pdf(binario_pdf, md5_pdf)
+            lista_md5_pdfs.append(md5_pdf)
+        return lista_md5_pdfs
 
-    def __get_md5(self, pdf):
-        md5 = hashlib.md5()
-        md5.update(pdf)
-        return str(md5.hexdigest())
+    def __obtem_response(self, url:str) -> str:
+        response = requests.get(url, headers = Scraper.headers)
+        return response.content
 
-    def get_lista_pdf_md5(self):
-        self.__gera_lista_arquivos_binarios_pdf()
-        for pdf in self.__lista_binarios_pdf:
-            md5 = self.__get_md5(pdf)
-            self.__lista_md5.append(md5)
-        return self.__lista_md5
+    def __salva_arquivo_pdf(self, binario_pdf:bytes, nome_arquivo:str) -> None:
+        with open(f'{nome_arquivo}.pdf', 'wb') as arquivo_pdf:
+            arquivo_pdf.write(binario_pdf)
 
-    def baixa_pdf_renomeado_com_md5(self):
-        i = 0
-        for pdf in self.__lista_binarios_pdf:
-            with open(self.__lista_md5[i] + ".pdf", "wb") as arquivo:
-                arquivo.write(pdf)
-            i += 1
+    def __obtem_md5_pdf(self, binario_pdf:bytes) -> str:
+        md5_pdf = hashlib.md5()
+        md5_pdf.update(binario_pdf)
+        return str(md5_pdf.hexdigest())
 
-    def inicia_busca(self):
-        if not self.__checa_existencia_de_diario():
-            return False
-        self.get_url_pdf()
-        return True
+    def imprime_log_md5_pdfs(self) -> None:
+        for md5 in self._lista_md5_pdfs:
+            print(md5)
 
-def main(args):
-    try:
-        data_informada = args[1]
-    except:
-        print("Nenhuma data foi informada.")
-        exit()
-    data_valida = valida_data(data_informada)
-    if not data_valida:
-        print("Data inválida!", end="\n")
-        exit()
-    busca_diario = Diario(data_valida)
-    if not busca_diario.inicia_busca():
-        print("Não há diários publicados na data informada.")
-        exit()
-    lista_md5 = busca_diario.get_lista_pdf_md5()
-    busca_diario.baixa_pdf_renomeado_com_md5()
-    for md5 in lista_md5:
-        print(md5)
+def main():
+    parser = argparse.ArgumentParser(prog='stf-scraping.py', description='Baixa os Diários oficiais do STF publicados em uma determinada data.')
+    parser.add_argument('data_informada', metavar = 'Data', type = str, help = 'Data de publicação (DD-MM-AAAA).')
+    args = parser.parse_args()
+
+    data_publicacao = args.data_informada
+    
+    busca = Scraper(data_publicacao)
+    lista_urls_pdf = busca.lista_urls_pdf
+
+    baixa_pdf = PDFDownloader(lista_urls_pdf)
+    baixa_pdf.imprime_log_md5_pdfs()
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    main()
