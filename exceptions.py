9feb00d56f57d58a21b69c2b1b6b667bcbcd01d5
Daniel Marcelino <daniel.azevedo@justicafacil.com.br>
2021-11-26 17:27:40 -0300

    Separa as exeções customizadas em um arquivo dedicado

---
+++ exceptions.py
@@ -0,0 +1,28 @@
+def data_invalida():
+    print('Data inválida!')
+    exit()
+
+
+def url_invalida_ou_indisponivel():
+    print('Url inválida ou insdisponível!')
+    exit()
+
+
+def indisponibilidade_temporaria():
+    print('Indisponibilidade temporária!')
+    exit()
+
+
+def diario_nao_publicado():
+    print('Diário não publicado!')
+    exit()
+
+
+def arquivo_incompativel_com_formato_pdf():
+    print('Arquivo incompativel com formato pdf!')
+    exit()
+
+
+def impossibilidade_em_salvar_pdf():
+    print('Impossibilidade em salvar pdf!')
+    exit()
