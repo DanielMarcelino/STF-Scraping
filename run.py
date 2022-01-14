b80469435c43014fcaac7bacb29dabeb972e8386
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-01-14 15:16:01 -0300

    Implementa o download mensal de diarios

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import argparse
+import calendar
 
 from src.request_get import RequestGet
 from src.pdf_downloader import PDFDownloader
@@ -10,23 +11,28 @@ def obtem_data_args() -> str:
         prog='stf-scraping.py',
         description='Baixa os Diários oficiais do STF publicados em uma determinada data.'
     )
-    parser.add_argument('data_informada', metavar='Data', type=str, help='Data de publicação (DD-MM-AAAA).')
+    parser.add_argument('data_informada', metavar='Data', type=str, help='Data de publicação (MM-AAAA).')
     args = parser.parse_args()
     return args.data_informada
 
 
 def main():
     data_publicacao = obtem_data_args()
-    instancia_de_requisicao = RequestGet()
-    instancia_de_busca = STFScraper(instancia_de_requisicao)
-    lista_urls_pdf = instancia_de_busca.obtem_lista_urls_pdfs(data_publicacao)
-    instancia_de_download_pdf = PDFDownloader(instancia_de_requisicao)
-    instancia_de_download_pdf.efetua_download(lista_urls_pdf)
-    lista_md5 = instancia_de_download_pdf.obtem_log_md5()
+    mes = int(data_publicacao.split('-')[0])
+    ano = int(data_publicacao.split('-')[1])
+    quantidade_de__dias_no_mes = calendar.monthrange(ano, mes)[1]
+    
+    lista_urls = []
+    instancia_de_busca = STFScraper()
 
-    for md5 in lista_md5:
-        print(md5)
+    for dia in range(1, quantidade_de__dias_no_mes + 1):
+        urls = instancia_de_busca.obtem_lista_urls_pdfs(f'{dia}/{mes}/{ano}')
+        if urls:
+            lista_urls.extend(urls)
 
+    instancia_de_download_pdf = PDFDownloader()
+    for url in lista_urls:
+        instancia_de_download_pdf.efetua_download(url)
 
 if __name__ == "__main__":
     main()
