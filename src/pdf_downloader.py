19876a92e08fd8b779756708b7493e021e7687b6
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-01-14 15:14:26 -0300

    Implementa o disparo de logs em pdf_downloader.py

--- src/pdf_downloader.py
+++ src/pdf_downloader.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from typing import List
+
+import requests
 
 from src.ferramentas import (
     cria_diretorio,
@@ -6,40 +9,36 @@ from src.ferramentas import (
     valida_pdf,
     gera_md5_hash
 )
-from src.request_get import RequestGet
 from src.exceptions import url_invalida_ou_indisponivel
 
 
 class PDFDownloader:
     DIRETORIO_PRINCIPAL = 'diarios'
+    HEADERS = {
+        'user-agent': (
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+        )
+    }
 
-    def __init__(self, instancia_de_requisicao: RequestGet) -> None:
-        self.instancia_de_requisicao = instancia_de_requisicao
+    def __init__(self) -> None:
         cria_diretorio(nome_diretorio=PDFDownloader.DIRETORIO_PRINCIPAL)
-        self.log_md5 = []
 
-    def obtem_log_md5(self) -> list:
-        return self.log_md5[:]
-
-    def efetua_download(self, lista_urls_pdfs: List[str]) -> list:
-        self._lista_vazia(lista_urls_pdfs)
-        for url in lista_urls_pdfs:
-            self._baixa_pdf(url)
+    def efetua_download(self, url: str) -> list:
+        self._baixa_pdf(url)
 
     def _baixa_pdf(self, url: str) -> None:
-        binario_pdf = self.instancia_de_requisicao.obtem_response(url)
-        valida_pdf(binario_pdf)
-        md5_pdf = gera_md5_hash(binario_pdf)
+        response = requests.get(url, headers=self.HEADERS, timeout=30)
+        url_split = url.split('=')
+        data_diario = url_split[2][:10]
+        numero_diario = url_split[4]
+        if response.status_code != 200 :
+            print(f'[{datetime.now()}] Erro {response.status_code} ao baixar diário nº {numero_diario} data {data_diario}')
+            return
+        md5_pdf = gera_md5_hash(response.content)
         salva_arquivo(
-            binario=binario_pdf,
+            binario=response.content,
             nome=f'{md5_pdf}.pdf',
             caminho=f'{PDFDownloader.DIRETORIO_PRINCIPAL}/'
         )
-        self._insere_md5_no_log(md5_pdf)
-
-    def _lista_vazia(self, lista_urls_pdfs: List[str]) -> bool:
-        if not lista_urls_pdfs:
-            url_invalida_ou_indisponivel()
-
-    def _insere_md5_no_log(self, md5: str) -> None:
-        self.log_md5.append(md5)
+        print(f'[{datetime.now()}] Diário nº {numero_diario} data {data_diario} baixado com sucesso md5 = {md5_pdf}')
