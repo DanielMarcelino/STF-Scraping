50c8cba9822d223d5fad7072a7de8c6ad9eba602
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-01-14 07:26:28 -0300

    Implementa disparo de logs em stf.py

--- src/stf.py
+++ src/stf.py
@@ -1,39 +1,42 @@
 from datetime import datetime
 
 from bs4 import BeautifulSoup
-
-from src.request_get import RequestGet
-from src.exceptions import data_invalida, diario_nao_publicado
+import requests
 
 
 class STFScraper:
+    HEADERS = {
+            'user-agent': (
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+            )
+        }
     URL_BASE = 'https://www.stf.jus.br/portal/diariojusticaeletronico/'
     PATH_URL = 'montarDiarioEletronico.asp?tp_pesquisa=0&dataP='
 
-    def __init__(self, custom_response: RequestGet) -> None:
-        self.custom_response = custom_response
-
     def obtem_lista_urls_pdfs(self, data: str) -> list:
-        self._valida_data(data)
-        lista_urls_pdfs = self._obtem_urls_dos_pdfs_da_pagina_html(data)
-        return lista_urls_pdfs[:]
-
-    def _obtem_urls_dos_pdfs_da_pagina_html(self, data: str) -> list:
         pagina_html = self._obtem_pagina_html(data)
+        if not pagina_html:
+            return None
         pagina_html = BeautifulSoup(pagina_html, 'html.parser')
         anchors = pagina_html.select('a[target]')
-        lista_urls_pdfs = [(f'{STFScraper.URL_BASE}{anchor.get("href").strip()}')for anchor in anchors]
-        if len(lista_urls_pdfs) == 0:
-            diario_nao_publicado()
+        if not anchors:
+            print(f'[{datetime.now()}] Não há diários publicados em {data}')
+            return None
+        lista_urls_pdfs = [(f'{STFScraper.URL_BASE}{anchor.get("href").strip()}') for anchor in anchors]
+        print(f'[{datetime.now()}] Foram encontrados {len(lista_urls_pdfs)} diários publicados em {data}')
         return lista_urls_pdfs
 
     def _obtem_pagina_html(self, data: str) -> bytes:
         url = f'{STFScraper.URL_BASE}{STFScraper.PATH_URL}{data}'
-        pagina_html = self.custom_response.obtem_response(url)
-        return pagina_html
+        response = requests.get(url, headers=self.HEADERS)
+        if response.status_code != 200:
+            print(f'[{datetime.now()}] Erro {response.status_code} ao buscar diários na data {data}')
+            return None
+        return response.content
 
-    def _valida_data(self, data: str) -> None:
-        try:
-            datetime.strptime(data, '%d-%m-%Y')
-        except Exception:
-            data_invalida()
+    # def _valida_data(self, data: str) -> None:
+    #     try:
+    #         datetime.strptime(data, '%d-%m-%Y')
+    #     except Exception:
+    #         data_invalida()
